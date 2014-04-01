0aed3880e0b837b5d5979065f0b056249f3b56a1
mpieler <mipie22@gmail.com>
2014-04-01 15:12:51 -0400

    random

--- Breezer/gen/com/example/breezer/R.java
+++ Breezer/gen/com/example/breezer/R.java
@@ -999,13 +999,12 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050043;
+        public static final int action_settings=0x7f050041;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int budgetTitle=0x7f05003f;
         public static final int checkbox=0x7f05002d;
-        public static final int classHelpTitle=0x7f050040;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
         public static final int default_activity_button=0x7f050028;
@@ -1028,8 +1027,7 @@ containing a value of this type.
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
-        public static final int optionsTitle=0x7f050041;
-        public static final int plannerTitle=0x7f050042;
+        public static final int optionsTitle=0x7f050040;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
