08062e7fab1d09ddffefa1e26f3b5eaef717a8e6
Amber Ransford <AmberRansford@gmail.com>
2014-04-01 23:36:24 -0400

    Created Symptoms Activity
    Also Added Basic Functionality

--- Breezer/gen/com/example/breezer/R.java
+++ Breezer/gen/com/example/breezer/R.java
@@ -999,8 +999,9 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050041;
+        public static final int action_settings=0x7f050044;
         public static final int activity_chooser_view_content=0x7f050025;
+        public static final int addSymptomsTitle=0x7f05003d;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
@@ -1021,13 +1022,14 @@ containing a value of this type.
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
-        public static final int mainMenuTextTitle=0x7f05003d;
+        public static final int logSymptomsTitle=0x7f050043;
+        public static final int mainMenuTextTitle=0x7f05003e;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
-        public static final int optionsTitle=0x7f05003e;
-        public static final int prescriptionTitle=0x7f05003f;
+        public static final int optionsTitle=0x7f05003f;
+        public static final int prescriptionTitle=0x7f050040;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1047,12 +1049,13 @@ containing a value of this type.
         public static final int showTitle=0x7f050006;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
-        public static final int symptomsTitle=0x7f050040;
+        public static final int symptomsTitle=0x7f050041;
         public static final int tabMode=0x7f050002;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
+        public static final int viewSymptomsTitle=0x7f050042;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1104,20 +1107,26 @@ containing a value of this type.
         public static final int abc_popup_menu_item_layout=0x7f030014;
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
-        public static final int activity_main=0x7f030017;
-        public static final int activity_new_prescription=0x7f030018;
-        public static final int activity_prescription=0x7f030019;
-        public static final int activity_symptoms=0x7f03001a;
-        public static final int fragment_main=0x7f03001b;
-        public static final int fragment_prescription=0x7f03001c;
-        public static final int fragment_symptoms=0x7f03001d;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001e;
+        public static final int activity_add_symptom=0x7f030017;
+        public static final int activity_main=0x7f030018;
+        public static final int activity_new_prescription=0x7f030019;
+        public static final int activity_prescription=0x7f03001a;
+        public static final int activity_symptoms=0x7f03001b;
+        public static final int activity_symtoms_log=0x7f03001c;
+        public static final int fragment_add_symptom=0x7f03001d;
+        public static final int fragment_main=0x7f03001e;
+        public static final int fragment_prescription=0x7f03001f;
+        public static final int fragment_symptoms=0x7f030020;
+        public static final int fragment_symtoms_log=0x7f030021;
+        public static final int support_simple_spinner_dropdown_item=0x7f030022;
     }
     public static final class menu {
-        public static final int main=0x7f0c0000;
-        public static final int new_prescription=0x7f0c0001;
-        public static final int prescription=0x7f0c0002;
-        public static final int symptoms=0x7f0c0003;
+        public static final int add_symptom=0x7f0c0000;
+        public static final int main=0x7f0c0001;
+        public static final int new_prescription=0x7f0c0002;
+        public static final int prescription=0x7f0c0003;
+        public static final int symptoms=0x7f0c0004;
+        public static final int symtoms_log=0x7f0c0005;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1161,6 +1170,7 @@ containing a value of this type.
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a0012;
+        public static final int add_symptoms=0x7f0a0019;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a0015;
         public static final int main_menu=0x7f0a000e;
@@ -1168,10 +1178,13 @@ containing a value of this type.
         public static final int prescription=0x7f0a000f;
         public static final int prescriptionName=0x7f0a0017;
         public static final int symptoms=0x7f0a0010;
+        public static final int title_activity_add_symptom=0x7f0a001b;
         public static final int title_activity_main=0x7f0a0013;
         public static final int title_activity_new_prescription=0x7f0a0016;
         public static final int title_activity_prescription=0x7f0a0014;
         public static final int title_activity_symptoms=0x7f0a0018;
+        public static final int title_activity_symtoms_log=0x7f0a001c;
+        public static final int view_symptoms=0x7f0a001a;
     }
     public static final class style {
         /** 
