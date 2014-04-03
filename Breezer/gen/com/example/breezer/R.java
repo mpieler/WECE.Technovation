f81a446060d22d8f35a782162f397993e49ff13d
Valentina Rendon <valentina_021992@hotmail.com>
2014-04-03 19:48:26 -0400

    gguidgfi
    bjhhiohio

--- Breezer/gen/com/example/breezer/R.java
+++ Breezer/gen/com/example/breezer/R.java
@@ -1180,24 +1180,30 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a0013;
         public static final int addPrescription=0x7f0a0012;
-        public static final int add_symptoms=0x7f0a001a;
+        public static final int add_symptoms=0x7f0a0020;
         public static final int app_name=0x7f0a000d;
-        public static final int edit_symptom=0x7f0a001f;
+        public static final int edit_symptom=0x7f0a0025;
         public static final int hello_world=0x7f0a0016;
         public static final int main_menu=0x7f0a000e;
         public static final int options=0x7f0a0011;
         public static final int prescription=0x7f0a000f;
+        public static final int prescriptionAmount=0x7f0a001c;
+        public static final int prescriptionFrequency=0x7f0a0019;
         public static final int prescriptionName=0x7f0a0018;
+        public static final int prescriptionRemaining=0x7f0a001e;
+        public static final int prescriptionSize=0x7f0a001a;
+        public static final int prescriptionStartdate=0x7f0a001d;
+        public static final int prescritionColor=0x7f0a001b;
         public static final int symptoms=0x7f0a0010;
-        public static final int title_activity_add_symptom=0x7f0a001c;
-        public static final int title_activity_edit_symptoms=0x7f0a0020;
+        public static final int title_activity_add_symptom=0x7f0a0022;
+        public static final int title_activity_edit_symptoms=0x7f0a0026;
         public static final int title_activity_main=0x7f0a0014;
         public static final int title_activity_new_prescription=0x7f0a0017;
-        public static final int title_activity_options=0x7f0a001e;
+        public static final int title_activity_options=0x7f0a0024;
         public static final int title_activity_prescription=0x7f0a0015;
-        public static final int title_activity_symptoms=0x7f0a0019;
-        public static final int title_activity_symtoms_log=0x7f0a001d;
-        public static final int view_symptoms=0x7f0a001b;
+        public static final int title_activity_symptoms=0x7f0a001f;
+        public static final int title_activity_symtoms_log=0x7f0a0023;
+        public static final int view_symptoms=0x7f0a0021;
     }
     public static final class style {
         /** 
