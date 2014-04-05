370334d0654403b4c27cf17808adc37ea6be3af2
mpieler <mipie22@gmail.com>
2014-04-05 19:25:28 -0400

    edited prescriptionColor name
    in strings.xml

--- Breezer/gen/com/example/breezer/R.java
+++ Breezer/gen/com/example/breezer/R.java
@@ -999,9 +999,9 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050046;
+        public static final int action_settings=0x7f05004c;
         public static final int activity_chooser_view_content=0x7f050025;
-        public static final int addPrescriptionTitle=0x7f050043;
+        public static final int addPrescriptionTitle=0x7f050049;
         public static final int addSymptomsTitle=0x7f05003d;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1024,7 +1024,7 @@ containing a value of this type.
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
-        public static final int logSymptomsTitle=0x7f050045;
+        public static final int logSymptomsTitle=0x7f05004b;
         public static final int mainMenuTextTitle=0x7f05003f;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
@@ -1053,11 +1053,17 @@ containing a value of this type.
         public static final int submit_area=0x7f050039;
         public static final int symptomsTitle=0x7f050042;
         public static final int tabMode=0x7f050002;
+        public static final int textView1=0x7f050043;
+        public static final int textView2=0x7f050044;
+        public static final int textView3=0x7f050045;
+        public static final int textView4=0x7f050046;
+        public static final int textView5=0x7f050047;
+        public static final int textView6=0x7f050048;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
-        public static final int viewSymptomsTitle=0x7f050044;
+        public static final int viewSymptomsTitle=0x7f05004a;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1188,12 +1194,12 @@ containing a value of this type.
         public static final int options=0x7f0a0011;
         public static final int prescription=0x7f0a000f;
         public static final int prescriptionAmount=0x7f0a001c;
+        public static final int prescriptionColor=0x7f0a001b;
         public static final int prescriptionFrequency=0x7f0a0019;
         public static final int prescriptionName=0x7f0a0018;
         public static final int prescriptionRemaining=0x7f0a001e;
         public static final int prescriptionSize=0x7f0a001a;
         public static final int prescriptionStartdate=0x7f0a001d;
-        public static final int prescritionColor=0x7f0a001b;
         public static final int symptoms=0x7f0a0010;
         public static final int title_activity_add_symptom=0x7f0a0022;
         public static final int title_activity_edit_symptoms=0x7f0a0026;
