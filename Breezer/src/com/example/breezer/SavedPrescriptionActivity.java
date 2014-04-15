013904c7513ef9a263b19dde2ae23798e9e71067
Valentina Rendon <valentina_021992@hotmail.com>
2014-04-14 20:51:50 -0400

    saveButton
    saveButton works

--- Breezer/src/com/example/breezer/SavedPrescriptionActivity.java
+++ Breezer/src/com/example/breezer/SavedPrescriptionActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.os.Build;
+import android.provider.BaseColumns;
 
 public class SavedPrescriptionActivity extends ActionBarActivity {
 
@@ -25,7 +26,7 @@ public class SavedPrescriptionActivity extends ActionBarActivity {
 
 	    // Get the message from the intent
 	    Intent intent = getIntent();
-	    String message = intent.getStringExtra(PrescriptionActivity.EXTRA_MESSAGE);
+	    String message = intent.getStringExtra(NewPrescriptionActivity.EXTRA_MESSAGE);
 
 	    // Create the text view
 	    TextView textView = new TextView(this);
@@ -75,5 +76,6 @@ public class SavedPrescriptionActivity extends ActionBarActivity {
 			return rootView;
 		}
 	}
+	
 	
 }
