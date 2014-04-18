236350b52f709dd5b17c072ae2bab7274c497c6f
mpieler <mipie22@gmail.com>
2014-04-17 22:32:59 -0400

    fixed

--- Breezer/gen/com/example/breezer/R.java
+++ Breezer/gen/com/example/breezer/R.java
@@ -1006,9 +1006,8 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050061;
+        public static final int action_settings=0x7f05005e;
         public static final int activity_chooser_view_content=0x7f050025;
-        public static final int add=0x7f05005c;
         public static final int addButton=0x7f050052;
         public static final int addPrescriptionTitle=0x7f050040;
         public static final int addSymptomsTitle=0x7f050058;
@@ -1019,7 +1018,6 @@ containing a value of this type.
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003f;
         public static final int default_activity_button=0x7f050028;
-        public static final int delete=0x7f05005d;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
@@ -1028,7 +1026,6 @@ containing a value of this type.
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
-        public static final int group=0x7f05005b;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
@@ -1037,9 +1034,9 @@ containing a value of this type.
         public static final int imageButton1=0x7f050046;
         public static final int imageButton2=0x7f050045;
         public static final int imageView1=0x7f050043;
-        public static final int item_name=0x7f050060;
-        public static final int item_subtitle=0x7f05005f;
-        public static final int item_text=0x7f05005e;
+        public static final int item_name=0x7f05005d;
+        public static final int item_subtitle=0x7f05005c;
+        public static final int item_text=0x7f05005b;
         public static final int listMode=0x7f050001;
         public static final int listViewFromDB=0x7f050041;
         public static final int list_item=0x7f050029;
@@ -1160,10 +1157,8 @@ containing a value of this type.
         public static final int fragment_prescription_database=0x7f030026;
         public static final int fragment_symptoms=0x7f030027;
         public static final int fragment_symtoms_log=0x7f030028;
-        public static final int fragment_test_database=0x7f030029;
-        public static final int item_layout=0x7f03002a;
-        public static final int progressbar=0x7f03002b;
-        public static final int support_simple_spinner_dropdown_item=0x7f03002c;
+        public static final int item_layout=0x7f030029;
+        public static final int support_simple_spinner_dropdown_item=0x7f03002a;
     }
     public static final class menu {
         public static final int add_symptom=0x7f0c0000;
