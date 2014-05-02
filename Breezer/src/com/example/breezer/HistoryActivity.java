63ea8cf206010094888c6916577e2fc644fd1b14
mpieler <mipie22@gmail.com>
2014-05-02 13:38:52 -0400

    updating screens

--- Breezer/src/com/example/breezer/HistoryActivity.java
+++ Breezer/src/com/example/breezer/HistoryActivity.java
@@ -9,32 +9,26 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.app.Activity;
-import android.widget.SeekBar;
-import android.widget.SeekBar.OnSeekBarChangeListener;
-import android.widget.TextView;
+import android.os.Build;
 
+public class HistoryActivity extends ActionBarActivity {
 
-public class AddSymptomActivity extends Activity{
-
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_add_symptom);
+		setContentView(R.layout.activity_history);
 
-		/*if (savedInstanceState == null) {
+		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
-		}*/
+		}
 	}
-	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.add_symptom, menu);
+		getMenuInflater().inflate(R.menu.history, menu);
 		return true;
 	}
 
@@ -61,10 +55,10 @@ public class AddSymptomActivity extends Activity{
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_add_symptom,
+			View rootView = inflater.inflate(R.layout.fragment_history,
 					container, false);
 			return rootView;
 		}
 	}
-}
 
+}
--- Breezer/src/com/example/breezer/MainActivity.java
+++ Breezer/src/com/example/breezer/MainActivity.java
@@ -89,4 +89,13 @@ public class MainActivity extends ActionBarActivity {
 	    //intent.putExtra(EXTRA_MESSAGE, message);
 	    startActivity(intent);
 	}
+	
+	/** Called when the user clicks Options link */
+	public void moveToHistory(View view) {
+	    Intent intent = new Intent(this, HistoryActivity.class);
+	    //EditText editText = (EditText) findViewById(R.id.edit_message);
+	    //String message = editText.getText().toString();
+	    //intent.putExtra(EXTRA_MESSAGE, message);
+	    startActivity(intent);
+	}
 }
